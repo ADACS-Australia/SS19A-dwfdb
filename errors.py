9242e0f086ea7fa28abdf770dc3279eb4cf811a4
Rebecca L <becca.lange84@gmail.com>
2019-06-24 09:50:13 +0800

    add draft custom errors

---
+++ errors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+class DWFFileNotSpecified(Exception):
+	pass
+
+class TableValueError(Exception):
+	pass
+
+class TableKeyError(Exception):
+	pass
+
+# class TableValueWarning(Warning):
+#     logging.warning()
