b8e3fee1969939db246d83b2fcecdd606e3a40a9
Rebecca Lange <becca.lange84@gmail.com>
2020-01-22 12:15:06 +0800

    update add_batch method and example

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
 import requests
 from .errors import *
-from astropy import units as u
-from astropy.coordinates import SkyCoord
+from astropy.io import ascii
+import pandas as pd
+import numpy as np
+#
+# from astropy import units as u
+# from astropy.coordinates import SkyCoord
 
 # Define a class
 class Client(object):
@@ -60,44 +64,81 @@ class Client(object):
 
     def add_batch(self, filename):
         """adds content of ascii file of records to the database"""
-        f = open(filename, 'r')
-        header = f.readline().split()
-        for line in f:
-            columns = line.split()
-            record_dict = dict(zip(header, columns))
-            # check if values are validated as per schema (integer and positive)
-            for key in ['ccd', 'mary_run', 'date', 'cand_num']:
-                if not record_dict.get(key).is_integer():
-                    raise TableValueError(key + " is not an integer")
-                else:
-                    record_dict[key] = int(record_dict[key])
-                if record_dict.get(key) < 0:
-                    raise TableValueError(key + " needs to be positive")
-                if record_dict.get(key) is None:
-                    raise TableValueError(key + " is empty")
+        batchfile = ascii.read(filename)
 
+        # turn ascii table to dictionary, example for one line
+        bkey = batchfile.colnames
+        for b in bkey:
+            batchfile.rename_column(b, b.lower())
+        batchfile.rename_column("maryid", "maryID")
+        bkey = batchfile.colnames
+        l = len(batchfile)
+        df = pd.DataFrame({'index': int(), 'reason': []})
+        for i in range(l):
+            record_dict = dict(zip(bkey, batchfile[bkey][i]))
+            for key in ['id', 'ccd', 'mary_run', 'date', 'cand_num']:
+                j = int(record_dict.get(key))
+                if j == record_dict.get(key):
+                    record_dict[key] = int(record_dict[key])
+                else:
+                    df = df.append({'index': int(i), 'reason': f"{key} not an int"}, ignore_index=True)
+                    # next
+                if record_dict.get(key) < 0:
+                    df = df.append({'index': int(i), 'reason': f"{key} needs to be positive"}, ignore_index=True)
+                    # next
+                if record_dict.get(key) is None:
+                    df = df.append({'index': int(i), 'reason': f"{key} is empty"}, ignore_index=True)
+                    # next
 
             # check if required values are numeric
-            for key in ['id','mag', 'emag', 'mjd', 'ra', 'dec']:
-                if type(record_dict.get(key)) is not float:
-                    raise TableValueError(key + " is not a float")
+            for key in ['mag', 'emag', 'mjd', 'ra', 'dec']:
+                if not np.issubdtype(type(record_dict.get(key)), np.number):
+
+                    df = df.append({'index': int(i), 'reason': f"{key} not a float"}, ignore_index=True)
+                    # raise TableValueError(key + " is not a float")
                 if record_dict.get(key) is None:
-                    raise TableValueError(key + " is empty")
+                    df = df.append({'index': int(i), 'reason': f"{key} is empty"}, ignore_index=True)
+                    # raise TableValueError(key + " is empty")
 
             # check if required values are strings and not NULL
             for key in ['maryID', 'sci_path', 'sub_path', 'temp_path']:
-                if type(record_dict.get(key)) is not str:
-                    raise TableValueError(key + " is not a string")
+                # if type(record_dict.get(key)) is not np.string:
+                if not np.issubdtype(type(record_dict.get(key)), np.str_):
+                    # a.dtype.type is np.string_
+                    df = df.append({'index': int(i), 'reason': f"{key} not a string"}, ignore_index=True)
+                    # raise TableValueError(key + " is not a string")
                 if record_dict.get(key) is None:
-                    raise TableValueError(key + " is empty")
+                    df = df.append({'index': int(i), 'reason': f"{key} is empty"}, ignore_index=True)
+                    # raise TableValueError(key + " is empty")
 
             if not 10.0 <= record_dict['mag'] <= 30.0:
-                raise TableValueError("mag is out of bounds")
+                df = df.append({'index': int(i), 'reason': f"mag is out of bounds: {record_dict['mag']}"}, ignore_index=True)
+                # raise TableValueError("mag is out of bounds")
             if not 0.0 <= record_dict['ra'] <= 360.0:
-                raise TableValueError("RA is out of bounds")
+                df = df.append({'index': int(i), 'reason': f"RA is out of bounds: {record_dict['ra']}"},
+                               ignore_index=True)
+                # raise TableValueError("RA is out of bounds")
             if not -90.0 <= record_dict['dec'] <= 90.0:
-                raise TableValueError("DEC is out of bounds")
-        f.close()
+                df = df.append({'index': int(i), 'reason': f"Dec is out of bounds: {record_dict['dec']}"}, ignore_index=True)
+                # raise TableValueError("DEC is out of bounds")
+
+            if any(df['index'] == i):
+                print(f"There are issues with row {i}, please check returned dataframe")
+                # next
+            else:
+                package = "web/run"
+                method = "create"
+                json_payload = record_dict
+                print(f"Sending request, row {i}")
+                try:
+                    r = requests.post(f"{self.api_url}/{package}/{method}", json=json_payload)
+                    if not r.status_code == requests.codes.OK:
+                        df = df.append({'index': int(i), 'reason': f"failed request status code {r.status_code}"}, ignore_index=True)
+                except Exception as e:
+                    df = df.append({'index': int(i), 'reason': f"failed request, check response {e}"}, ignore_index=True)
+                    # print("failed request, check response ", e)
+        self.batch = df
+
 
     def add_post(self, author=None, maryid=None, body=None):
         """adds a dictionary of comments to the database"""
